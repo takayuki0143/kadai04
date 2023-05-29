415dd9f18ff979a6a4cd9d9961ffa2a74abddac6
takayuki0143 <takayuki30973@gmail.com>
2023-05-29 23:02:48 +0900

    検索ボタンを作成しました

--- js/app.js
+++ js/app.js
@@ -4,29 +4,34 @@ const send = document.getElementById("send");
 // ボタンをクリックしたらイベントが発火する→入力したものが保存される
 send.addEventListener('click', () => {
     const textarea = document.getElementById("textarea");
-    let t = textarea.value;
+    let t = textarea;
 
     // valueの中身を複数にしたい
-    let meala = document.getElementById("text_1").value;
-    let mealb = document.getElementById("text_2").value;
-    let mealc = document.getElementById("text_3").value;
-    let meald = document.getElementById("text_4").value;
+    let menua = document.getElementById("text_1");
+    let menub = document.getElementById("text_2");
+    let menuc = document.getElementById("text_3");
+    let menud = document.getElementById("text_4");
 
     let meals = {
-        meal1: meala,
-        meal2: mealb,
-        meal3: mealc,
-        meal4: meald
+        "data1": menua.value,
+        "data2": menub.value,
+        "data3": menuc.value,
+        "data4": menud.value
     };
+    console.log(meals);
 
     // localstrageに複数valueを保存する
-    localStorage.setItem(t,JSON.stringify(meals));
-
+    localStorage.setItem(t.value,JSON.stringify(meals));
+    textarea.value = "";
+    menua.value ='';
+    menub.value ='';
+    menuc.value ='';
+    menud.value ='';
 });
 
     // データを取得
     const load = document.getElementById("load");
-load.addEventListener('click', () => {
+    load.addEventListener('click', () => {
     const list = document.getElementById('list');
     list.innerHTML = ""; // 既存のリストをクリアする
 
@@ -42,8 +47,40 @@ load.addEventListener('click', () => {
         `;
         list.innerHTML += html;
     }
- 
+
+    });
+
+   
+
+    // 検索機能もつけたい
+    //もしすでに登録してあるlocalstrageのkeyと一致するものがあれば、Valueを返してほしい
+
+    // 登録作業
+    const r = document.getElementById("resister");
+    r.addEventListener('click', () => {
+        const re = document.getElementById("resis").value;
+        const t = document.getElementById("val").value;
+        localStorage.setItem(re,t);
+        resis.value ="";
+        val.value ="";
     });
     
+    // 検索作業
+    const searchInput =document.getElementById("searchInput");
+    const searchBtn =document.getElementById("searchBtn");
+    const searchResult =document.getElementById("searchResult"); 
+
+    searchBtn.addEventListener('click',() =>{
+        const search = document.getElementById("searchInput");
+        const content = search.value;
+        const result = localStorage.getItem(content);
+        search.value ="";
+        if (result) {
+            searchResult.textContent =  result;
+        } else {
+            searchResult.textContent = "キーが見つかりません";
+        }
+
+    });
 
 
